df3f5f3f38418d2259fd3759ee50801c7d0b1b12
Declan Grant <declangrant612@gmail.com>
2023-08-22 20:13:11 -0600

    Replace facebook with twitter

--- js/script.js
+++ js/script.js
@@ -177,10 +177,6 @@ if (cursor !== "default") {
     $("body").css({"cursor": `url(${root}assets/cursors/${cursors[cursor]}), auto`});
 }
 
-$(document).ready(() => {
-    $("a[href*='https://elfsight.com/event-calendar-widget/?utm_source=websites&utm_medium=clients&utm_content=event-calendar&utm_term=www.pasa.website&utm_campaign=free-widget']").parent("div").remove();
-});
-
 // Enable email tooltip
 $(document).ready(function() {
     $('#email').tooltip();
@@ -203,8 +199,8 @@ $("#instagram").click(() => {
     window.open("https://www.instagram.com/ucalgarypasa");
 });
 
-$("#facebook").click(() => {
-    window.open("https://www.fb.com/UCalgaryPASA");
+$("#twitter").click(() => {
+    window.open("https://www.twitter.com/UCalgaryPASA");
 });
 
 // Load content without refreshing
