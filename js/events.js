14c4fe8422f8410d2bc0c592da977d475e05149e
Declan Grant <declangrant612@gmail.com>
2023-09-01 11:16:00 -0600

    Use google api

--- js/events.js
+++ js/events.js
@@ -1,88 +1,131 @@
-$(document).ready(function () {
-    $.get("../data/events.yaml", null, (response) => {
-        // Read events from YAML file
-        var events = jsyaml.load(response);
-        // console.log("Events", events);
+const upcomingEvents = "#upcoming";
+const pastEvents = "#previous";
 
-        // Sort events by date
-        var groups = _.groupBy(events, function(event) { 
-            return moment(event.start).format('YYYY-MM-DD'); 
-        });
-        days = _.sortBy(Object.keys(groups));
+const now = new Date().toISOString();
 
-        // console.log("Groups:", groups);
-        // console.log("Event Days:", days);
+var apiKey;
+var eventsCalendarId;
 
-        // Define HTML ids
-        const upcomingEvents = "#events";
-        const pastEvents = "#past-events";
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+function month(date) {
+    return monthNames[date.getMonth()];
+};
 
-        // Define HTML for events
-        const addEvents = (events, id) => {
-            // Add events
-            events.forEach(event => {
-                event.starttime = moment(event.start).format("h:mm a");
-                event.endtime = moment(event.end).format("h:mm a");
-
-                // console.log(event);
-
-                // Append event details
-                if (event.title) {
-                    $('<span><strong>' + event.title + '</strong>  </span>').appendTo(id);
-                }
-                if (event.venue) {
-                    $('<br /><span class="p-location location h-card vcard"><span>' + event.venue + '</span> ' + '</span>').appendTo(id);
-                }
-                if (!event.allday) {
-                    $(`<span> - ${event.starttime}</span>`).appendTo(id);
-                }
-                if (event.summary) {
-                    $('<br /><span class="p-name summary">' + event.summary + '</span>').appendTo(id);
-                }
-                if (event.website) {
-                    $('<br /><a href="' + event.website + '" class="ml small"> <i class="icon-link-ext"></i> Link Here</a>').appendTo(id);
-                }
-
-                $("<br />").appendTo(id);
-                $('</div>').appendTo(id);
-                $("<br />").appendTo(id);
-            });
+function nth(date) {
+    day = date.getDate()
+    if (day > 3 && day < 21) {
+        ord = "th";
+    } else {
+        switch (day % 10) {
+            case 1:
+                ord = "st";
+                break;
+            case 2:
+                ord = "nd";
+                break;
+            case 3:
+                ord = "rd";
+                break;
+            default:
+                ord = "th";
+                break;
         };
+    };
+    return `${day}${ord}`
+};
 
-        // Add upcoming events in chronological order     
-        days.forEach(function (day) {
-            if (moment(day).isAfter(moment().subtract('days', 1))) {
-                // Add date heading
-                $('<h5><time class="dt-start dtstart">' + moment(day).format('MMMM Do, YYYY') + '</time></h5>').appendTo(upcomingEvents);
+function timeStr(date) {
+    var options = {
+        hour: "numeric",
+        hour12: true
+    };
+    if (date.getMinutes() != 0) {
+        options.minute = "numeric"
+    };
+    return date.toLocaleString("en-US", options);
+};
 
-                // Sort the day's events by time
-                groups[day].sort(function(a, b) {
-                    start = moment(a.start).format("h:mm a");
-                    end = moment(b.start).format("h:mm a");
-                    return start.localeCompare(end);
-                });
+function getTimezone(day) {
+    tzStr = new Date(day).toLocaleString("en", {timeZone: "America/Edmonton", timeZoneName: "short"});
+    return tzStr.split(" ").slice(-1)[0];
+};
 
-                // Add events for this day
-                addEvents(groups[day], upcomingEvents);
-            }
-        });
+function addEvents(divId, maxResults = 50, continuationToken = null) {
+    
+    var apiQuery = "https://www.googleapis.com/calendar/v3/calendars/"
+                 + `${eventsCalendarId}${encodeURIComponent('@')}group.calendar.google.com/events`
+                 + `?maxResults=${maxResults}`
+                 + "&orderBy=startTime"
+                 + "&singleEvents=true"
+                 + `&timeMin=${now}`
+                 + `&key=${apiKey}`;
+    
+    if (continuationToken !== null) {
+        apiQuery += `&continuationToken=${continuationToken}`;
+    };
 
-        /// Add past events in reverse chronological order 
-        days.reverse().forEach(function (day) {
-            if (moment(day).isBefore(moment().subtract('days', 1))) {
-                // Add date heading
-                $('<h5><time class="dt-start dtstart">' + moment(day).format('MMMM Do, YYYY') + '</time></h5>').appendTo(pastEvents);
+    fetch(apiQuery)
+        .then((response) => response.json())
+        .then((data) => {
+            data.items.forEach(event => {
+                $("<div>").appendTo(divId);
+                $(`<span><strong>${event.summary}</strong></span><br>`).appendTo(divId);
+                
+                var displayDatetime = "";
+                // all day event
+                if ("date" in event.start) {
+                    var startTz = getTimezone(event.start.date);
+                    var start = new Date(`${event.start.date} ${startTz}`);
+                    var endTz = getTimezone(event.start.date);
+                    var end = new Date(`${event.end.date} ${endTz}`);
+                    end.setDate(end.getDate() - 1);     // google sets all day events to end the next day
 
-                // Sort the day's events by time
-                groups[day].sort(function(a, b) {
-                    start = moment(a.start).format("h:mm a");
-                    end = moment(b.start).format("h:mm a");
-                    return start.localeCompare(end);
-                });
+                    longspan = false;   // if event spans months or years
+                    if (start.getFullYear() != end.getFullYear()) {
+                        displayDatetime += `${month(start)} ${nth(start)}, ${start.getFullYear()} - `;
+                        longspan = true;
+                    } else if (start.getMonth() != end.getMonth()) {
+                        displayDatetime += `${month(start)} ${nth(start)} - `;
+                        longspan = true;
+                    };
+                    displayDatetime += `${month(end)} `;
+                    if (start.getDate() != end.getDate() && !longspan) {
+                        displayDatetime += `${nth(start)} - `;
+                    };
+                    displayDatetime += `${nth(end)}, ${end.getFullYear()}`;
 
-                // Add events for this day
-                addEvents(groups[day], pastEvents);
-            }
-        });
+                // hourly event
+                } else {
+                    var start = new Date(event.start.dateTime);
+                    var end = new Date(event.end.dateTime);
+                    displayDatetime = `${timeStr(start)} - ${timeStr(end)}, ${month(end)} ${nth(end)}, ${end.getFullYear()}`
+                };
+
+                $(`<span>${displayDatetime}</span><br>`).appendTo(divId);
+                
+                if (event.location) {
+                    $(`<span>${event.location}</span><br>`).appendTo(divId);
+                };
+
+                if (event.description) {
+                    $(`<span>${event.description}</span><br>`).appendTo(divId);
+                };
+                
+                $("</div><br>").appendTo(divId)
+            });
+    });
+
+}
+
+$(document).ready(function() {
+
+    // load calendar data
+    fetch("/data/googleapi.json")
+        .then((response) => response.json())
+        .then((config) => {
+            apiKey = config.apiKey;
+            eventsCalendarId = config.eventsCalendarId;
+            addEvents(upcomingEvents)
     });
 });
